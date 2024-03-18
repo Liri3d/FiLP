3fc76292512d811bddb243ddc98eeecaea749cc6
unknown <vlad.ogon.2004@gmail.com>
2024-03-18 18:00:26 +0300

    Сообщение коммита

--- recursion.pl
+++ recursion.pl
@@ -1,4 +1,4 @@
-% Предикат находит максимальное из чисел
+% Предикат находит максимальное из чисел. (УНИФ - X, Y, Z | НЕУНИФ - U)
 max(X, Y, Z, U) :-
     X >= Y,
     X >= Z,
@@ -12,7 +12,7 @@ max(X, Y, Z, U) :-
     Z >= Y,
     U = Z.
 
-% Предикат находит факторил первого аргумента при помощи рекурсии вверх
+% Предикат находит факторил первого аргумента при помощи рекурсии вверх. (УНИФ - N | НЕУНИФ - X, N1, X1)
 fact(0, 1).
 
 fact(N, X) :-
@@ -21,7 +21,7 @@ fact(N, X) :-
     fact(N1, X1),
     X is N * X1.
 
-% Предикат находит факторил первого аргумента при помощи рекурсии вниз
+% Предикат находит факторил первого аргумента при помощи рекурсии вниз. (УНИФ - N | НЕУНИФ - X, N1, X1, Acc, Acc1)
 factd(N, X) :-
     N > 0,
     fact_helper(N, 1, X).
@@ -32,7 +32,7 @@ fact_helper(N, Acc, X) :-
     Acc1 is Acc * N,
     fact_helper(N1, Acc1, X).
 
-% Предикат находит сумму цифр числа с помощью рекурсии вверх
+% Предикат находит сумму цифр числа с помощью рекурсии вверх. (УНИФ - N | НЕУНИФ - Sum, Sum1, Remainder, Quotien)
 sum_of_digits(N, Sum) :-
     N < 10,
     Sum is N.
@@ -43,7 +43,7 @@ sum_of_digits(N, Sum) :-
     sum_of_digits(Quotient, Sum1),
     Sum is Sum1 + Remainder.
 
-% Предикат находит сумму цифр числа с помощью рекурсии вниз
+% Предикат находит сумму цифр числа с помощью рекурсии вниз. (УНИФ - N | НЕУНИФ - Sum, Acc, Acc1, Remainder, Quotien)
 sum_of_digits_d(N, Sum) :-
     sum_of_digits_helper(N, 0, Sum).
 
@@ -54,7 +54,7 @@ sum_of_digits_helper(N, Acc, Sum) :-
     Acc1 is Acc + Remainder,
     sum_of_digits_helper(Quotient, Acc1, Sum).
 
-% Предикат проверяет, является ли число свободным от квадратов
+% Предикат проверяет, является ли число свободным от квадратов. (УНИФ - N | НЕУНИФ - X)
 is_square_free(1).
 is_square_free(N) :-
     N > 1,
@@ -65,21 +65,21 @@ has_square_divisor(N, X) :-
     (N mod (X * X) =:= 0 ;
     has_square_divisor(N, X+1)).
 
-% Предикат чтения списка с клавиатуры
+% Предикат чтения списка с клавиатуры. (УНИФ - user_input | НЕУНИФ - List, Codes, Atom, Atoms)
 read_list(List) :-
     read_line_to_codes(user_input, Codes),
     atom_codes(Atom, Codes),
     atomic_list_concat(Atoms, ' ', Atom),
     maplist(atom_number, Atoms, List).
 
-% Предикат вывода списка на экран
+% Предикат вывода списка на экран. (УНИФ - X | НЕУНИФ - Xs)
 write_list([]).
 write_list([X|Xs]) :-
     write(X),
     write(' '),
     write_list(Xs).
 
-% Предикат проверяет, является ли Sum суммой элементов списка или записывает в эту переменную сумму элементов.
+% Предикат проверяет, является ли Sum суммой элементов списка или записывает в эту переменную сумму элементов. (УНИФ - List | НЕУНИФ - X, Xs, Sum, RestSum)
 sum_list_d([], 0).
 sum_list_d([X|Xs], Sum) :-
     sum_list_d(Xs, RestSum),
@@ -92,27 +92,27 @@ sum_list_down :-
     writeln('Сумма элементов списка:'),
     write(Sum).
 
-% Предикат суммирования элементов списка c исп. предикатов
+% Предикат суммирования элементов списка c исп. предикатов. (УНИФ - List | НЕУНИФ - X, Xs, Sum, Sum1)
 sum_list_el([], 0).
 sum_list_el([X|Xs], Sum) :-
     sum_list_el(Xs, Sum1),
     Sum is X + Sum1.
 
-sum_list_up :-
+sum_list :-
     write('Введите список элементов (через пробел): '),
     read_list(List),
     sum_list_el(List, Sum),
     write('Сумма элементов списка: '),
     write(Sum).
 
-% Предикат проверяет, является ли Sum суммой элементов списка или записывает в эту переменную сумму элементов. Рекурсия вверх
+% Предикат проверяет, является ли Sum суммой элементов списка или записывает в эту переменную сумму элементов. Рекурсия вверх. (УНИФ - X | НЕУНИФ - Xs, Sum, Sum1)
 sum_list_up([], 0).
 sum_list_up([X|Xs], Sum) :-
     sum_list_up(Xs, Sum1),
     Sum is Sum1 + X.
 
 
-% Предикат, который удаляет все элементы, сумма цифр которых равна данной.
+% Предикат, который удаляет все элементы, сумма цифр которых равна данной. (УНИФ - List, Sum | НЕУНИФ - X, Xs, NewList, SumX, Rest)
 remove_by_sum([], _, []).
 remove_by_sum([X|Xs], Sum, NewList) :-
     sum_of_digits(X, SumX),
